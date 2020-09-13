3bead9bbe749fe9ce12a3be032f716ed43bb0ba0
pooyaaf <pooya1380@gmail.com>
2020-09-13 19:41:29 +0430

    Updates

---
+++ precache-manifest.d823ec3b124a0c7bf706d9ad161e54eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1d58377c20f098009ca5ca6a268b7c",
+    "url": "/Hulu-Clone/index.html"
+  },
+  {
+    "revision": "dc07a84b1941b930a311",
+    "url": "/Hulu-Clone/static/css/main.7f2d4bff.chunk.css"
+  },
+  {
+    "revision": "5971841f90788b3b42f9",
+    "url": "/Hulu-Clone/static/js/2.ac5dc3ea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Hulu-Clone/static/js/2.ac5dc3ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc07a84b1941b930a311",
+    "url": "/Hulu-Clone/static/js/main.22b02a14.chunk.js"
+  },
+  {
+    "revision": "7f17e45edea30b0b0e0c",
+    "url": "/Hulu-Clone/static/js/runtime-main.9ee240a6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Hulu-Clone/precache-manifest.d823ec3b124a0c7bf706d9ad161e54eb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Hulu-Clone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
